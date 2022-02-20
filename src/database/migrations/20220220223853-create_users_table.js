a816c61652913bf882606086e8eba4287b13fb43
Jeremy Krovitz <jeremykrovitz@Jeremys-MBP.hitronhub.home>
2022-02-20 17:51:22 -0600

    Add code through video 30

---
+++ src/database/migrations/20220220223853-create_users_table.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+    up: function(queryInterface, Sequelize) {
+        return queryInterface.createTable('Users', {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            email: {
+                type: Sequelize.STRING(100),
+                allowNull: false,
+                unique: true,
+                // We cannot include validate in a migration; Postgres is not capable of email validation
+            },
+            password: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            username: {
+                type: Sequelize.STRING(50),
+                unique: true,
+            },
+            firstName: {
+                type: Sequelize.STRING(50),
+            },
+            lastName: {
+                type: Sequelize.STRING(50),
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
+    down: function(queryInterface, Sequelize) {
+        return queryInterface.dropTable('Users');
+    }
+};
