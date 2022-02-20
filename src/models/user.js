900dfa9664fcd4bfb44ba225f106305854b956ff
Jeremy Krovitz <jeremykrovitz@Jeremys-MBP.hitronhub.home>
2022-02-20 13:21:36 -0600

    Add code through video 23

---
+++ src/models/user.js
@@ -0,0 +1,62 @@
+import {Model, DataTypes} from 'sequelize'
+
+export default (sequelize) => {
+	class User extends Model {
+		static associate(models) {
+			User.RefreshToken = User.hasOne(models.RefreshToken)
+			User.Roles = User.hasMany(models.Roles)
+		}
+
+		static async hashPassword(password) { }
+		
+		static async createNewUser({ email, password, roles, username, firstName, lastName, refreshToken }) { }
+	}
+	
+	// Defining the properties of the user model
+	User.init({
+		email: {
+			type: DataTypes.STRING(100),
+			allowNull: false,
+			unique: true,
+			validate: {
+				isEmail: {
+					msg: 'Not a valid email address',
+				},
+			},
+		},
+		password: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		username: {
+			type: DataTypes.STRING(50),
+			unique: true,
+			validate: {
+				len: {
+					args: [2, 50], // min and max number of characters
+					msg: 'Username must contain between 2 and 50 characters',
+				},
+			},	
+		},
+		firstName: {
+			type: DataTypes.STRING(50),
+			validate: {
+				len: {
+					args: [3, 50], // min and max number of characters
+					msg: 'First name must contain between 3 and 50 characters',
+				},
+			},
+		},
+		lastName: {
+			type: DataTypes.STRING(50),
+			validate: {
+				len: {
+					args: [3, 50], // min and max number of characters
+					msg: 'Last name must contain between 3 and 50 characters',
+				},
+			},
+		},
+	}, { sequelize, modelName: 'User' }
+	)
+	return User;
+}
