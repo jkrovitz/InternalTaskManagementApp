34e7a1286d60b3d284b4127084ae49b100e581ff
Jeremy Krovitz <jeremykrovitz@Jeremys-MBP.hitronhub.home>
2022-02-20 14:44:46 -0600

    Add code through video 25

--- src/models/user.js
+++ src/models/user.js
@@ -14,7 +14,41 @@ export default (sequelize) => {
             return bcrypt.hash(password, environment.saltRounds) // first argument is a string which will be our password and the second argument is going to be the saltOrRounds, which can be a string or number and it returns a Promise that resolves to a string, which will be the hashstring. We will get the value for the SaltOrRounds by importing environment from '../config/environment' at the top of the file.
         }
 
-        static async createNewUser({ email, password, roles, username, firstName, lastName, refreshToken }) {}
+        static async createNewUser({
+            email,
+            password,
+            roles,
+            username,
+            firstName,
+            lastName,
+            refreshToken
+        }) {
+            return sequelize.transaction(async() => {
+                let rolesToSave = []; // empty array
+
+                // roles = ["customer", "admin"]
+                //rolesToSave = [{role: "customer"}, {role: "admin"}]
+                if (roles && Array.isArray(roles)) { // if roles is defined and if roles is an array
+                    rolesToSave = roles.map(role => ({ role })); // rolesToSave = roles.map and we're going to convert the role to an object
+                }
+
+                await User.create({
+                    email,
+                    password,
+                    username,
+                    firstName,
+                    lastName,
+                    RefreshToken: { token: refreshToken }, // this is how we create an associated refreshToken with this user. We pass an object specifying the token.
+                    Roles: rolesToSave, // needs to be an array of objects; the parameter roles is just going to be an array of strings, so we need to convert that array of string to this array of objects
+                }, {
+                    include: [User.RefreshToken, User.Roles]
+                })
+
+                // we have added all of the properties to the users table
+                // we need to find a way to create this user with roles and a refreshToken and that's where User.RefreshToken = User.hasOne(models.RefreshToken) and User.Roles = User.hasMany(models.Roles) are going to help us.
+
+            })
+        }
     }
 
     // Defining the properties of the user model
