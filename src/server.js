b278159abef33deb2360d23917237aa1cd71d27b
Jeremy Krovitz <jeremykrovitz@Jeremys-MBP.hitronhub.home>
2022-02-20 00:24:13 -0600

    Add code through video 20

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import './config' // we put import './config' at the top of the file because this is going to execute first 
+import Database from './database'
+import environment from './config/environment' // and then we need to execute or import the environment variables after we have loaded all of our environment variables 
+import dbConfig from './config/database'
+
+// immediately invoked function expression
+(async () => {
+	try {
+		const db = new Database(environment.nodeEnv, dbConfig) // create new database and pass in environment and dbConfig
+		await db.connect()
+
+	} catch (err) {
+		console.error('Something went wrong when initializing the server:\n', err.stack) // Logging message and printing the error stack
+	}
+})()
