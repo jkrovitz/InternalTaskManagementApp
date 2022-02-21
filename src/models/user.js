ea7cf428e63d5516c8137a3c74382007739a7bc3
Jeremy Krovitz <jeremykrovitz@Jeremys-MacBook-Pro.local>
2022-02-21 15:39:56 -0600

    Add code through video 49

--- src/models/user.js
+++ src/models/user.js
@@ -3,136 +3,136 @@ import bcrypt from 'bcrypt';
 import environment from '../config/environment'
 
 export default (sequelize) => {
-    class User extends Model {
-        static associate(models) {
-            User.RefreshToken = User.hasOne(models.RefreshToken)
-            User.Tasks = User.hasMany(models.Task)
-        }
-
-        // Takes a password and hashes it with the bcrypt algorithm
-        static async hashPassword(password) {
-            return bcrypt.hash(password, environment.saltRounds) // first argument is a string which will be our password and the second argument is going to be the saltOrRounds, which can be a string or number and it returns a Promise that resolves to a string, which will be the hashstring. We will get the value for the SaltOrRounds by importing environment from '../config/environment' at the top of the file.
-        }
-
-        // const result = await createNewUser(...)
-        //result === undefined       
-        static async createNewUser({
-            email,
-            password,
-            tasks,
-            username,
-            firstName,
-            lastName,
-            refreshToken
-        }) {
-            return sequelize.transaction(() => {
-                let tasksToSave = []; // empty array
-
-                // tasks = ["task1", "task2"]
-                //rolesToSave = [{task: "task1"}, {task: "task2"}]
-                if (tasks && Array.isArray(tasks)) { // if tasks is defined and if tasks is an array
-                    tasksToSave = tasks.map(task => ({ task })); // rolesToSave = tasks.map and we're going to convert the task to an object
-                }
-
-                return User.create({
-                    email,
-                    password,
-                    username,
-                    firstName,
-                    lastName,
-                    RefreshToken: { token: refreshToken }, // this is how we create an associated refreshToken with this user. We pass an object specifying the token.
-                    Tasks: tasksToSave, // needs to be an array of objects; the parameter tasks is just going to be an array of strings, so we need to convert that array of string to this array of objects
-                }, {
-                    include: [User.RefreshToken, User.Tasks]
-                })
-
-                // we have added all of the properties to the users table
-                // we need to find a way to create this user with tasks and a refreshToken and that's where User.RefreshToken = User.hasOne(models.RefreshToken) and User.Tasks = User.hasMany(models.Tasks) are going to help us.
-
-            })
-        }
+  class User extends Model {
+    static associate(models) {
+      User.RefreshToken = User.hasOne(models.RefreshToken)
+      User.Tasks = User.hasMany(models.Task)
     }
 
-    // Defining the properties of the user model
-    User.init({
-        email: {
-            type: DataTypes.STRING(100),
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: {
-                    msg: 'Not a valid email address',
-                },
-                notNull: {
-                    msg: 'Email is required',
-                }
-            },
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        username: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-            unique: true,
-            validate: {
-                len: {
-                    args: [2, 50], // min and max number of characters
-                    msg: 'Username must contain between 2 and 50 characters',
-                },
-                notNull: {
-                    msg: 'Username is required'
-                }
-            },
-        },
-        firstName: {
-            type: DataTypes.STRING(50),
-            validate: {
-                len: {
-                    args: [3, 50], // min and max number of characters
-                    msg: 'First name must contain between 3 and 50 characters',
-                },
-            },
-        },
-        lastName: {
-            type: DataTypes.STRING(50),
-            validate: {
-                len: {
-                    args: [3, 50], // min and max number of characters
-                    msg: 'Last name must contain between 3 and 50 characters',
-                },
-            },
-        },
-    }, {
-        sequelize,
-        modelName: 'User',
-        defaultScope: { attributes: { exclude: ['password'] } },
-        scopes: {
-            withPassword: {
-                attributes: { include: ['password'] }
-            }
-        }
-    })
-
-    // user = await User.findOne({where: {email: 'test@example.com'}})
-    // user.email, user.username, user.firstName, user.lastName, user.password (even if the password is hashed, we don't want to expose that password to anyone. When we need the password, we're going to pass in scope, and say, "hey, include in the query the password but otherwise, we're not going to include the password")
-    // user.comparePasswords('test123#') // if this is the same as the hashed password, true is returned otherwise false is returned
-    User.prototype.comparePasswords = async function(password) {
-        return bcrypt.compare(password, this.password) // returns a promise of whether the passed password is the same as the hashed password
+    // Takes a password and hashes it with the bcrypt algorithm
+    static async hashPassword(password) {
+      return bcrypt.hash(password, environment.saltRounds) // first argument is a string which will be our password and the second argument is going to be the saltOrRounds, which can be a string or number and it returns a Promise that resolves to a string, which will be the hashstring. We will get the value for the SaltOrRounds by importing environment from '../config/environment' at the top of the file.
     }
 
-    // This hook is going to guarantee that every user we save, the password is going to be hashed, so we will NEVER be saving a plain text password.
-    User.beforeSave(async(user, options) => {
-        if (user.password) {
-            const hashedPassword = await User.hashPassword(user.password)
-            user.password = hashedPassword
+    // const result = await createNewUser(...)
+    //result === undefined       
+    static async createNewUser({
+      email,
+      password,
+      tasks,
+      username,
+      firstName,
+      lastName,
+      refreshToken
+    }) {
+      return sequelize.transaction(() => {
+        let tasksToSave = []; // empty array
+
+        // tasks = ["task1", "task2"]
+        //rolesToSave = [{task: "task1"}, {task: "task2"}]
+        if (tasks && Array.isArray(tasks)) { // if tasks is defined and if tasks is an array
+          tasksToSave = tasks.map(task => ({ task })); // rolesToSave = tasks.map and we're going to convert the task to an object
         }
-    })
 
-    User.afterCreate((user, options) => {
-        delete user.dataValues.password
-    })
+        return User.create({
+          email,
+          password,
+          username,
+          firstName,
+          lastName,
+          RefreshToken: { token: refreshToken }, // this is how we create an associated refreshToken with this user. We pass an object specifying the token.
+          Tasks: tasksToSave, // needs to be an array of objects; the parameter tasks is just going to be an array of strings, so we need to convert that array of string to this array of objects
+        }, {
+          include: [User.RefreshToken, User.Tasks]
+        })
 
-    return User;
+        // we have added all of the properties to the users table
+        // we need to find a way to create this user with tasks and a refreshToken and that's where User.RefreshToken = User.hasOne(models.RefreshToken) and User.Tasks = User.hasMany(models.Tasks) are going to help us.
+
+      })
+    }
+  }
+
+  // Defining the properties of the user model
+  User.init({
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          msg: 'Not a valid email address',
+        },
+        notNull: {
+          msg: 'Email is required',
+        }
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: {
+          args: [2, 50], // min and max number of characters
+          msg: 'Username must contain between 2 and 50 characters',
+        },
+        notNull: {
+          msg: 'Username is required'
+        }
+      },
+    },
+    firstName: {
+      type: DataTypes.STRING(50),
+      validate: {
+        len: {
+          args: [3, 50], // min and max number of characters
+          msg: 'First name must contain between 3 and 50 characters',
+        },
+      },
+    },
+    lastName: {
+      type: DataTypes.STRING(50),
+      validate: {
+        len: {
+          args: [3, 50], // min and max number of characters
+          msg: 'Last name must contain between 3 and 50 characters',
+        },
+      },
+    },
+  }, {
+    sequelize,
+    modelName: 'User',
+    defaultScope: { attributes: { exclude: ['password'] } },
+    scopes: {
+      withPassword: {
+        attributes: { include: ['password'] }
+      }
+    }
+  })
+
+  // user = await User.findOne({where: {email: 'test@example.com'}})
+  // user.email, user.username, user.firstName, user.lastName, user.password (even if the password is hashed, we don't want to expose that password to anyone. When we need the password, we're going to pass in scope, and say, "hey, include in the query the password but otherwise, we're not going to include the password")
+  // user.comparePasswords('test123#') // if this is the same as the hashed password, true is returned otherwise false is returned
+  User.prototype.comparePasswords = async function(password) {
+    return bcrypt.compare(password, this.password) // returns a promise of whether the passed password is the same as the hashed password
+  }
+
+  // This hook is going to guarantee that every user we save, the password is going to be hashed, so we will NEVER be saving a plain text password.
+  User.beforeSave(async(user, options) => {
+    if (user.password) {
+      const hashedPassword = await User.hashPassword(user.password)
+      user.password = hashedPassword
+    }
+  })
+
+  User.afterCreate((user, options) => {
+    delete user.dataValues.password
+  })
+
+  return User;
 }
--- tests/tests-helpers.js
+++ tests/tests-helpers.js
@@ -1,54 +1,66 @@
 import '../src/config' // import necessary environment variables
 import Database from '../src/database'
 import dbConfig from '../src/config/database'
+import request from 'supertest'
 
 let db
 
 export default class TestHelpers {
-    static async startDb() {
-        db = new Database('test', dbConfig); // dbConfig contains the test object which contains information about the test database
-        await db.connect()
-        return db // in case some of our tests need access to this instance of the database
+  static async startDb() {
+    db = new Database('test', dbConfig); // dbConfig contains the test object which contains information about the test database
+    await db.connect()
+    return db // in case some of our tests need access to this instance of the database
+  }
+
+  static async stopDb() {
+    await db.disconnect();
+  }
+
+  // Method to reset database; each time we run a test, we are going to drop all of the tables and have a test with a clean database, so we run the syncDb command before each test
+  static async syncDb() {
+    await db.sync();
+  }
+
+  static async createNewUser(option = {}) {
+    const models = require('../src/models').default; // the default export is this models object
+    const {
+      email = 'test@example.com',
+        password = 'Test123#',
+        tasks = ['work on report', 'work on write up'],
+        username = 'test',
+        firstName = 'Jeremy',
+        lastName = 'Krovitz',
+        refreshToken = 'test-refresh-token',
+
+    } = option
+    const { User } = models
+    const data = {
+      email,
+      password,
+      tasks,
+      username,
+      firstName,
+      lastName,
+      refreshToken,
     }
+    return User.createNewUser(data)
 
-    static async stopDb() {
-        await db.disconnect();
-    }
+  }
 
-    // Method to reset database; each time we run a test, we are going to drop all of the tables and have a test with a clean database, so we run the syncDb command before each test
-    static async syncDb() {
-        await db.sync();
-    }
+  static getApp() {
+    const App = require('../src/app').default
+    return new App().getApp()
+  }
 
-    static async createNewUser(option = {}) {
-        const models = require('../src/models').default; // the default export is this models object
-        const {
-            email = 'test@example.com',
-                password = 'Test123#',
-                tasks = ['work on report', 'work on write up'],
-                username = 'test',
-                firstName = 'Jeremy',
-                lastName = 'Krovitz',
-                refreshToken = 'test-refresh-token',
-
-        } = option
-        const { User } = models
-        const data = {
-            email,
-            password,
-            tasks,
-            username,
-            firstName,
-            lastName,
-            refreshToken,
-        }
-        return User.createNewUser(data)
-
-    }
-
-    static getApp() {
-        const App = require('../src/app').default
-        return new App().getApp()
-    }
-
+  //We want this method to call the endpoint, which will return the access token and the refresh token
+  static async registerNewUser(options = {}) {
+    const {
+      email = 'test@example.com',
+        password = 'Test123#',
+        endpoint = '/v1/register'
+    } = options
+    return request(TestHelpers.getApp())
+      .post(endpoint)
+      .send({ email, password })
+  }
 }
