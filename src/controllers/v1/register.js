bb6ebe9d293f169432a98c2b19aa7cf60add494f
Jeremy Krovitz <jeremykrovitz@Jeremys-MBP.hitronhub.home>
2022-02-21 00:21:27 -0600

    Add code through video 46

---
+++ src/controllers/v1/register.js
@@ -0,0 +1,32 @@
+import { Router } from 'express'
+import models from '../../models'
+import asyncWrapper from '../../utils/asyncWrapper'
+import JWTUtils from '../../utils/jwt-utils'
+
+const router = Router()
+const { User } = models
+
+router.post('/register', asyncWrapper(async(req, res) => {
+    const { email } = req.body
+    const user = awaitUser.findOne({ where: { email } })
+
+    if (user) {
+        return res.status(200).send({ sucess: false, message: 'User already exists' })
+    }
+
+    const payload = { email }
+    const accessToken = JWTUtils.generateAccessToken(payload) // we are generating an access token for a new user
+    const refreshToken = JWTUtils.generateRefreshToken(payload) // we are generating a refresh token for a new user
+    await User.createNewUser({...req.body, refreshToken }) // pass all of the day in the request body and the refreshToken
+
+    // if this doesn't fail
+    return res.status(200).send({
+        success: true,
+        message: 'User successfully registered',
+        data: {
+            accessToken,
+            refreshToken
+        }
+    })
+
+}))
