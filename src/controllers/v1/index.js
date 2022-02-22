ef487e00835eac987016f0e22354985adacc54bb
Jeremy Krovitz <jeremykrovitz@Jeremys-MacBook-Pro.local>
2022-02-21 21:07:39 -0600

    Add code through video 52

--- src/controllers/v1/index.js
+++ src/controllers/v1/index.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 import registerRouter from './register'
 import loginRouter from './login'
+import tokenRouter from './token'
 
 const router = Router()
 
 // this is where we are going to register all of our controllers like the register, login, logout, and token controllers
 router.use(registerRouter)
 router.use(loginRouter)
+router.use(tokenRouter)
 
 export default router;
